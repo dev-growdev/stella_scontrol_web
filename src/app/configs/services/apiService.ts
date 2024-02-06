2ae451e59c171c0e0d849d0bc18ffa33f2bc4024
misasoares <misasoaresms2@gmail.com>
2024-02-06 08:20:00 -0300

    feature: configura axios para requisições globais

---
+++ src/app/configs/services/apiService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiService = axios.create({
+    baseURL: "http://localhost:8080/api/",
+});
+
+export interface ResponseAPI {
+    success?: boolean;
+    code?: number;
+    message?: string;
+    data?: any;
+}
+
+apiService.interceptors.request.use((config) => {
+    const userToken = localStorage.getItem("token");
+    const token = `Bearer ${userToken}`;
+    config.headers.authorization = token ? token : "";
+    return config;
+});
+export default apiService;
