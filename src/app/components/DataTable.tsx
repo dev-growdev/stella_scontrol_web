fb3e5cd3316d47a967b250adaab79f90b6a4878c
misasoares <misasoaresms2@gmail.com>
2024-02-09 15:29:40 -0300

    feature: altera o formato do data

--- src/app/components/DataTable.tsx
+++ src/app/components/DataTable.tsx
@@ -24,45 +24,29 @@ import * as React from 'react';
 
 interface Data {
     id: number;
-    calories: number;
-    carbs: number;
-    fat: number;
     name: string;
-    protein: number;
+    createdAt: Date;
+    enable: boolean
 }
 
 function createData(
     id: number,
     name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+    createdAt: Date,
+    enable: boolean
 ): Data {
     return {
         id,
         name,
-        calories,
-        fat,
-        carbs,
-        protein,
+        createdAt,
+        enable
     };
 }
 
 const rows = [
-    createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-    createData(2, 'Donut', 452, 25.0, 51, 4.9),
-    createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-    createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-    createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-    createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-    createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-    createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-    createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-    createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-    createData(13, 'Oreo', 437, 18.0, 63, 4.0),
+    createData(1, 'Cupcake', new Date(Date.now()), true),
+    createData(2, 'Donut', new Date(Date.now()), false),
+    createData(3, 'Eclair', new Date(Date.now()), true)
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -113,32 +97,20 @@ const headCells: readonly HeadCell[] = [
         id: 'name',
         numeric: false,
         disablePadding: true,
-        label: 'Dessert (100g serving)',
+        label: 'Nome',
     },
     {
-        id: 'calories',
-        numeric: true,
+        id: 'createdAt',
+        numeric: false,
         disablePadding: false,
-        label: 'Calories',
+        label: 'Criado em:',
     },
     {
-        id: 'fat',
+        id: 'enable',
         numeric: true,
         disablePadding: false,
-        label: 'Fat (g)',
-    },
-    {
-        id: 'carbs',
-        numeric: true,
-        disablePadding: false,
-        label: 'Carbs (g)',
-    },
-    {
-        id: 'protein',
-        numeric: true,
-        disablePadding: false,
-        label: 'Protein (g)',
-    },
+        label: 'Status',
+    }
 ];
 
 interface EnhancedTableProps {
@@ -166,7 +138,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
                 {headCells.map((headCell) => (
                     <TableCell
                         key={headCell.id}
-                        align={headCell.numeric ? 'right' : 'left'}
+                        align="right"
                         padding={headCell.disablePadding ? 'none' : 'normal'}
                         sortDirection={orderBy === headCell.id ? order : false}
                     >
@@ -249,7 +221,7 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
 
 export default function EnhancedTable() {
     const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
+    const [orderBy, setOrderBy] = React.useState<keyof Data>('name');
     const [selected, setSelected] = React.useState<number | null>(null);
     const [page, setPage] = React.useState(0);
     const [dense, setDense] = React.useState(false);
@@ -355,6 +327,7 @@ export default function EnhancedTable() {
                                             />
                                         </TableCell>
                                         <TableCell
+                                            align='right'
                                             component="th"
                                             id={labelId}
                                             scope="row"
@@ -362,10 +335,8 @@ export default function EnhancedTable() {
                                         >
                                             {row.name}
                                         </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
-                                        <TableCell align="right">{row.fat}</TableCell>
-                                        <TableCell align="right">{row.carbs}</TableCell>
-                                        <TableCell align="right">{row.protein}</TableCell>
+                                        <TableCell align='right'>{`${new Date(row.createdAt).getDate()}/ ${new Date(row.createdAt).getMonth() + 1}/${new Date(row.createdAt).getFullYear()}`}</TableCell>
+                                        <TableCell align='right'>{row.enable.toString()}</TableCell>
                                     </TableRow>
                                 );
                             })}
