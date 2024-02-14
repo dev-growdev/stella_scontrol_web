3bf32680b8c5e44ca6eeb77f1afdbd2e648d40d0
misasoares <misasoaresms2@gmail.com>
2024-02-14 17:50:37 -0300

    refactor: limpa e simplifica data table

--- src/app/components/DataTable.tsx
+++ src/app/components/DataTable.tsx
@@ -1,371 +1,150 @@
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import DeleteIcon from '@mui/icons-material/Delete';
 import { TextField } from '@mui/material';
 import Box from '@mui/material/Box';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
-import Switch from '@mui/material/Switch';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-import { alpha } from '@mui/material/styles';
-import { visuallyHidden } from '@mui/utils';
 import * as React from 'react';
-
-interface Data {
-    id: number;
-    name: string;
-    createdAt: Date;
-    enable: boolean
-}
-
-function createData(
-    id: number,
-    name: string,
-    createdAt: Date,
-    enable: boolean
-): Data {
-    return {
-        id,
-        name,
-        createdAt,
-        enable
-    };
-}
-
-const rows = [
-    createData(1, 'Cupcake', new Date(Date.now()), true),
-    createData(2, 'Donut', new Date(Date.now()), false),
-    createData(3, 'Eclair', new Date(Date.now()), true)
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-    order: Order,
-    orderBy: Key,
-): (
-    a: { [key in Key]: number | string },
-    b: { [key in Key]: number | string },
-) => number {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
-
-interface HeadCell {
-    disablePadding: boolean;
-    id: keyof Data;
-    label: string;
-    numeric: boolean;
-}
-
-const headCells: readonly HeadCell[] = [
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: true,
-        label: 'Nome',
-    },
-    {
-        id: 'createdAt',
-        numeric: false,
-        disablePadding: false,
-        label: 'Criado em:',
-    },
-    {
-        id: 'enable',
-        numeric: true,
-        disablePadding: false,
-        label: 'Status',
-    }
-];
+import { Category } from '../main/categories/categoriesSlice';
 
 interface EnhancedTableProps {
-    numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
-    onSelectAllClick?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    order: Order;
-    orderBy: string;
-    rowCount: number;
+	selectItem?: (item: Category) => void;
+	categoriesData?: Category[];
 }
 
-function EnhancedTableHead(props: EnhancedTableProps) {
-    const { order, orderBy, numSelected, rowCount, onRequestSort } =
-        props;
-    const createSortHandler =
-        (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
-            onRequestSort(event, property);
-        };
+export default function EnhancedTable({ selectItem, categoriesData }: EnhancedTableProps) {
+	const [page, setPage] = React.useState(0);
+	const [rowsPerPage, setRowsPerPage] = React.useState(5);
+	const [searchValue, setSearchValue] = React.useState('');
+	const [selectedItemId, setSelectedItemId] = React.useState<string | null>(null);
 
-    return (
-        <TableHead>
-            <TableRow>
-                <TableCell padding="checkbox">
-                </TableCell>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id}
-                        align="right"
-                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        <TableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'asc'}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box component="span" sx={visuallyHidden}>
-                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                </Box>
-                            ) : null}
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
+	React.useEffect(() => {
+		if (selectedItemId !== null) {
+			const findItem = categoriesData.find(item => item.uid === selectedItemId);
+			selectItem(findItem);
+		}
+	}, [selectedItemId]);
 
-interface EnhancedTableToolbarProps {
-    numSelected: number;
-    item?: any
-    searchTable?: (arg: string) => void
-}
+	const handleChangePage = (event: unknown, newPage: number) => {
+		setPage(newPage);
+	};
 
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-    const { numSelected, item, searchTable } = props;
-    const [searchValue, setSearchValue] = React.useState("");
+	const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
 
-    function handleSearch(e) {
-        const newValue = e.target.value;
-        setSearchValue(newValue);
-        searchTable(newValue);
-    }
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(e.target.value);
+	};
 
-    return (
-        <Toolbar
-            sx={{
-                pl: { sm: 2 },
-                pr: { xs: 1, sm: 1 },
-                ...(numSelected > 0 && {
-                    bgcolor: (theme) =>
-                        alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-                }),
-            }}
-        >
-            {numSelected > 0 ? (
-                <TextField sx={{ flex: '1 1 100%' }} variant='standard' label="Altere o nome da categoria" value={item.name} />
-            ) : (
-                <Typography
-                    sx={{ flex: '1 1 100%' }}
-                    variant="h6"
-                    id="tableTitle"
-                    component="div"
-                >
-                    Categorias
-                </Typography>
-            )}
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                <TextField onChange={(e) => handleSearch(e)} value={searchValue} label="Pesquise por categorias" fullWidth InputProps={{
-                    endAdornment: (
-                        <FuseSvgIcon sx={{ marginRight: '10px', color: theme => theme.palette.secondary.light, }} >
-                            heroicons-outline:search
-                        </FuseSvgIcon>
-                    )
-                }} />
-            )}
-        </Toolbar>
-    );
-}
+	const handleRowClick = (itemId: string) => {
+		setSelectedItemId(itemId === selectedItemId ? null : itemId);
+	};
 
-export default function EnhancedTable() {
-    const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof Data>('name');
-    const [selected, setSelected] = React.useState<number | null>(null);
-    const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [selectedItem, setSelectedItem] = React.useState<any | null>(null);
-    const [searchValue, setSearchValue] = React.useState("");
+	const filteredRows = searchValue
+		? categoriesData.filter(row => row.name.toLowerCase().includes(searchValue.toLowerCase()))
+		: categoriesData;
 
-    React.useEffect(() => {
-        console.log(selectedItem) //aqui mostra o item selecionado
-    }, [selectedItem])
+	const sortedRows = filteredRows.slice().reverse(); // Reversing the array to display the most recent items first
 
-    const handleRequestSort = (
-        event: React.MouseEvent<unknown>,
-        property: keyof Data,
-    ) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const handleClick = (event: React.MouseEvent<unknown>, data: any) => {
-        setSelected(selected === data.id ? null : data.id);
-        setSelectedItem(data);
-    };
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-    const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDense(event.target.checked);
-    };
-
-    const isSelected = (id: number) => selected === id;
-
-    const filterRows = (rows, searchValue) => {
-        return rows.filter(row =>
-            row.name.toLowerCase().includes(searchValue.toLowerCase())
-        );
-    };
-
-    const visibleRows = React.useMemo(
-        () =>
-            stableSort(filterRows(rows, searchValue), getComparator(order, orderBy)).slice(
-                page * rowsPerPage,
-                page * rowsPerPage + rowsPerPage,
-            ),
-        [order, orderBy, page, rowsPerPage, searchValue]
-    );
-
-    const emptyRows =
-        rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-    return (
-        <Box sx={{ width: '100%' }}>
-            <Paper sx={{ width: '100%', mb: 2 }}>
-                <EnhancedTableToolbar
-                    item={selectedItem}
-                    numSelected={selected}
-                    searchTable={setSearchValue}
-                />
-                <TableContainer>
-                    <Table
-                        sx={{ minWidth: 750 }}
-                        aria-labelledby="tableTitle"
-                        size={dense ? 'small' : 'medium'}
-                    >
-                        <EnhancedTableHead
-                            numSelected={selected}
-                            order={order}
-                            orderBy={orderBy}
-                            onRequestSort={handleRequestSort}
-                            rowCount={rows.length}
-                        />
-                        <TableBody>
-                            {visibleRows.map((row, index) => {
-                                const isItemSelected = isSelected(+row.id);
-                                const labelId = `enhanced-table-checkbox-${index}`;
-
-                                return (
-                                    <TableRow
-                                        hover
-                                        onClick={(event) => handleClick(event, row)}
-                                        role="checkbox"
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        key={row.id}
-                                        selected={isItemSelected}
-                                        sx={{ cursor: 'pointer' }}
-                                    >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox
-                                                color="primary"
-                                                checked={isItemSelected}
-                                                inputProps={{
-                                                    'aria-labelledby': labelId,
-                                                }}
-                                            />
-                                        </TableCell>
-                                        <TableCell
-                                            align='right'
-                                            component="th"
-                                            id={labelId}
-                                            scope="row"
-                                            padding="none"
-                                        >
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align='right'>{`${new Date(row.createdAt).getDate()}/ ${new Date(row.createdAt).getMonth() + 1}/${new Date(row.createdAt).getFullYear()}`}</TableCell>
-                                        <TableCell align='right'>{row.enable.toString()}</TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                            {emptyRows > 0 && (
-                                <TableRow
-                                    style={{
-                                        height: (dense ? 33 : 53) * emptyRows,
-                                    }}
-                                >
-                                    <TableCell colSpan={6} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={rows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </Paper>
-            <FormControlLabel
-                control={<Switch checked={dense} onChange={handleChangeDense} />}
-                label="Dense padding"
-            />
-        </Box>
-    );
+	return (
+		<Box sx={{ width: '100%' }}>
+			<Paper sx={{ width: '100%', mb: 2 }}>
+				<Toolbar>
+					<Typography
+						variant="h6"
+						component="div"
+					>
+						Categorias cadastradas
+					</Typography>
+					<TextField
+						onChange={handleSearch}
+						value={searchValue}
+						label="Pesquise por categorias"
+						fullWidth
+					/>
+				</Toolbar>
+				<TableContainer>
+					<Table>
+						<TableBody>
+							{sortedRows
+								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+								.map((row, index) => (
+									<TableRow
+										key={row.uid}
+										sx={{
+											cursor: 'pointer',
+											backgroundColor:
+												row.uid === selectedItemId
+													? theme => theme.palette.primary.main
+													: 'inherit'
+										}}
+										onClick={() => handleRowClick(row.uid)}
+									>
+										<TableCell
+											component="th"
+											scope="row"
+											sx={{
+												color:
+													row.uid === selectedItemId
+														? theme => theme.palette.background.paper
+														: 'inherit'
+											}}
+										>
+											{row.name}
+										</TableCell>
+										<TableCell
+											sx={{
+												color:
+													row.uid === selectedItemId
+														? theme => theme.palette.background.paper
+														: 'inherit'
+											}}
+										>{`${new Date(row.createdAt).getDate()}/${
+											new Date(row.createdAt).getMonth() + 1
+										}/${new Date(row.createdAt).getFullYear()}`}</TableCell>
+										<TableCell
+											sx={{
+												color:
+													row.uid === selectedItemId
+														? theme => theme.palette.background.paper
+														: 'inherit'
+											}}
+										>
+											{row.enable.toString()}
+										</TableCell>
+										<TableCell
+											sx={{
+												color:
+													row.uid === selectedItemId
+														? theme => theme.palette.background.paper
+														: theme => theme.palette.secondary.light
+											}}
+										>
+											<FuseSvgIcon>heroicons-outline:pencil</FuseSvgIcon>
+										</TableCell>
+									</TableRow>
+								))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+				<TablePagination
+					rowsPerPageOptions={[5, 10, 25]}
+					component="div"
+					count={sortedRows.length}
+					rowsPerPage={rowsPerPage}
+					page={page}
+					onPageChange={handleChangePage}
+					onRowsPerPageChange={handleChangeRowsPerPage}
+				/>
+			</Paper>
+		</Box>
+	);
 }
