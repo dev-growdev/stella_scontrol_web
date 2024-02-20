b481b88539544c4153819ede75657ce313cd8281
misasoares <misasoaresms2@gmail.com>
2024-02-20 08:42:08 -0300

    doc: altera rota de api para env

--- src/app/configs/service/api.service.ts
+++ src/app/configs/service/api.service.ts
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: "http://localhost:8080/api",
+	baseURL: process.env.REACT_APP_API_URL
 });
 
 export interface ResponseAPI {
-    success?: boolean;
-    code?: number;
-    message?: string;
-    data?: any;
+	success?: boolean;
+	code?: number;
+	message?: string;
+	data?: any;
 }
 
-apiService.interceptors.request.use((config) => {
-    const userToken = localStorage.getItem("token");
-    const token = `Bearer ${userToken}`;
-    config.headers.authorization = token ? token : "";
-    return config;
+apiService.interceptors.request.use(config => {
+	const userToken = localStorage.getItem('token');
+	const token = `Bearer ${userToken}`;
+	config.headers.authorization = token ? token : '';
+	return config;
 });
 export default apiService;
