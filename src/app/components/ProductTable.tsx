6384c210389138a0b171cf706f4fb94978b77daf
misasoares <misasoaresms2@gmail.com>
2024-02-20 17:03:16 -0300

    refactor: ajusta table para os padrões da tableData

--- src/app/components/ProductTable.tsx
+++ src/app/components/ProductTable.tsx
@@ -15,7 +15,6 @@ import {
 	TextField
 } from '@mui/material';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -26,9 +25,9 @@ import { ChangeEvent, useEffect, useState } from 'react';
 import { Product, ProductsType } from '../main/products/productsSlice';
 
 interface ProductTableProps {
-	selectItem?: (item: Product | null) => void;
-	productsData?: ProductsType | { products: []; loading: false };
-	handleStatus?: (item: Product) => void;
+	selectItem: (item: Product | null) => void;
+	productsData: ProductsType | { products: []; loading: false };
+	handleStatus: (item: Product) => void;
 }
 
 export default function ProductTable({ selectItem, productsData, handleStatus }: ProductTableProps) {
@@ -42,7 +41,10 @@ export default function ProductTable({ selectItem, productsData, handleStatus }:
 	const openMenuStatus = Boolean(anchorStatusMenu);
 
 	useEffect(() => {
-		console.log(productsData, 'aaa')
+		console.log('CONSOLE DE DENTRO DO PRODUCT TABLE', productsData);
+	}, [productsData]);
+
+	useEffect(() => {
 		if (selectedItemId !== null && productsData.products) {
 			const findItem = productsData.products.find(item => item.uid === selectedItemId);
 			selectItem(findItem || null);
@@ -74,6 +76,7 @@ export default function ProductTable({ selectItem, productsData, handleStatus }:
 						filterByStatus === 'all' ||
 						(filterByStatus === 'active' && row.enable) ||
 						(filterByStatus === 'inactive' && !row.enable);
+
 					return matchesSearch && matchesStatus;
 			  })
 			: [];
@@ -95,143 +98,138 @@ export default function ProductTable({ selectItem, productsData, handleStatus }:
 
 	return (
 		<Box sx={{ width: '100%' }}>
-			<Paper sx={{ width: '100%', mb: 2 }}>
-				<Toolbar>
-					<div className="flex flex-col sm:flex-row w-full items-center gap-24">
-						<Typography
-							variant="h6"
-							component="div"
+			<Toolbar>
+				<div className="flex flex-col sm:flex-row w-full items-center gap-24">
+					<Typography
+						variant="h6"
+						component="div"
+					>
+						Produtos cadastrados
+					</Typography>
+					<TextField
+						onChange={handleSearch}
+						value={searchValue}
+						label="Pesquise por categorias"
+						fullWidth
+					/>
+					<div>
+						<Button
+							id="basic-button"
+							aria-controls={openMenuStatus ? 'basic-menu' : undefined}
+							aria-haspopup="true"
+							aria-expanded={openMenuStatus ? 'true' : undefined}
+							onClick={handleOpenStatusMenu}
+							className="w-full sm:w-144 pl-60 pr-64"
+							variant="contained"
+							startIcon={<FuseSvgIcon>heroicons-outline:filter</FuseSvgIcon>}
 						>
-							Categorias cadastradas
-						</Typography>
-						<TextField
-							onChange={handleSearch}
-							value={searchValue}
-							label="Pesquise por categorias"
-							fullWidth
-						/>
-						<div>
-							<Button
-								id="basic-button"
-								aria-controls={openMenuStatus ? 'basic-menu' : undefined}
-								aria-haspopup="true"
-								aria-expanded={openMenuStatus ? 'true' : undefined}
-								onClick={handleOpenStatusMenu}
-								className="w-full sm:w-144 pl-60 pr-64"
-								variant="contained"
-								startIcon={<FuseSvgIcon>heroicons-outline:filter</FuseSvgIcon>}
-							>
-								{filterByStatus === 'all' && 'FILTRAR'}
-								{filterByStatus === 'active' && 'ATIVOS'}
-								{filterByStatus === 'inactive' && 'INATIVOS'}
-							</Button>
+							{filterByStatus === 'all' && 'FILTRAR'}
+							{filterByStatus === 'active' && 'ATIVOS'}
+							{filterByStatus === 'inactive' && 'INATIVOS'}
+						</Button>
 
-							<Menu
-								id="basic-menu"
-								anchorEl={anchorStatusMenu}
-								open={openMenuStatus}
-								onClose={handleCloseStatusMenu}
-								MenuListProps={{
-									'aria-labelledby': 'basic-button'
-								}}
-							>
-								<MenuItem onClick={() => handleCloseStatusMenu('all')}>Todos</MenuItem>
-								<MenuItem onClick={() => handleCloseStatusMenu('active')}>Ativos</MenuItem>
-								<MenuItem onClick={() => handleCloseStatusMenu('inactive')}>Inativos</MenuItem>
-							</Menu>
-						</div>
+						<Menu
+							id="basic-menu"
+							anchorEl={anchorStatusMenu}
+							open={openMenuStatus}
+							onClose={handleCloseStatusMenu}
+							MenuListProps={{
+								'aria-labelledby': 'basic-button'
+							}}
+						>
+							<MenuItem onClick={() => handleCloseStatusMenu('all')}>Todos</MenuItem>
+							<MenuItem onClick={() => handleCloseStatusMenu('active')}>Ativos</MenuItem>
+							<MenuItem onClick={() => handleCloseStatusMenu('inactive')}>Inativos</MenuItem>
+						</Menu>
 					</div>
-				</Toolbar>
-				<TableContainer>
-					<Table>
-						<TableHead className="flex justify-between">
-							<TableCell>Nome</TableCell>
-							<TableCell>Status</TableCell>
-							<TableCell>Ações</TableCell>
-						</TableHead>
-						<TableBody>
-							{productsData.loading ? (
-								<Box className="flex justify-center items-center">
-									<CircularProgress color="primary" />
-								</Box>
-							) : (
-								sortedProducts
-									.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-									.map(row => (
-										<TableRow
-											className="flex justify-between"
-											key={row.uid}
-										>
-											<TableCell
-												className="min-w-160"
-												component="th"
-												scope="row"
-											>
-												{row.name}
-											</TableCell>
+				</div>
+			</Toolbar>
+			<TableContainer>
+				<Table>
+					<TableHead className="flex justify-between">
+						<TableCell>Nome</TableCell>
+						<TableCell>Categoria</TableCell>
+						<TableCell>Status</TableCell>
+						<TableCell>Ações</TableCell>
+					</TableHead>
+					<TableBody>
+						{productsData.loading ? (
+							<Box className="flex justify-center items-center">
+								<CircularProgress color="primary" />
+							</Box>
+						) : (
+							sortedProducts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+								<TableRow
+									className="flex justify-between"
+									key={row.uid}
+								>
+									<TableCell
+										className="min-w-200"
+										component="th"
+										scope="row"
+									>
+										{row.name}
+									</TableCell>
 
-											<TableCell className="min-w-160 flex justify-end">
-												<Stack
-													direction="row"
-													spacing={1}
-												>
-													<Chip
-														className="min-w-64"
-														color={row.enable ? 'primary' : 'error'}
-														label={row.enable ? 'Ativo' : 'Inativo'}
-													/>
-												</Stack>
-											</TableCell>
-											<TableCell
-												className="min-w-224 flex justify-end"
-												sx={{
-													color: theme => theme.palette.secondary.light
-												}}
+									<TableCell className="min-w-200 flex justify-center">
+										{row.category.name ?? 'deu xabu'}
+									</TableCell>
+									<TableCell className="min-w-200 flex justify-end">
+										<Stack
+											direction="row"
+											spacing={1}
+										>
+											<Chip
+												className="min-w-64"
+												color={row.enable ? 'primary' : 'error'}
+												label={row.enable ? 'Ativo' : 'Inativo'}
+											/>
+										</Stack>
+									</TableCell>
+									<TableCell
+										className="min-w-224 flex justify-end"
+										sx={{
+											color: theme => theme.palette.secondary.light
+										}}
+									>
+										<div className="flex w-full justify-between">
+											<FuseSvgIcon
+												onClick={() => handleRowClick(row.uid)}
+												className="w-32 mr-20 cursor-pointer"
 											>
-												<div className="flex w-full justify-between">
-													<div
-														onClick={() => handleRowClick(row.uid)}
-														className="w-32 mr-20 "
-													>
-														<FuseSvgIcon
-															sx={{
-																cursor: 'pointer'
-															}}
-														>
-															heroicons-outline:pencil
-														</FuseSvgIcon>
-													</div>
-													<FormGroup>
-														<FormControlLabel
-															control={
-																<Switch
-																	name="enable"
-																	checked={row.enable}
-																	onChange={() => handleStatus(row)}
-																/>
-															}
-															label={row.enable ? 'Inativar' : 'Ativar'}
+												heroicons-outline:pencil
+											</FuseSvgIcon>
+
+											<FormGroup>
+												<FormControlLabel
+													control={
+														<Switch
+															name="enable"
+															checked={row.enable}
+															onChange={() => handleStatus(row)}
 														/>
-													</FormGroup>
-												</div>
-											</TableCell>
-										</TableRow>
-									))
-							)}
-						</TableBody>
-					</Table>
-				</TableContainer>
-				<TablePagination
-					rowsPerPageOptions={[5, 10, 25]}
-					component="div"
-					count={sortedProducts.length}
-					rowsPerPage={rowsPerPage}
-					page={page}
-					onPageChange={handleChangePage}
-					onRowsPerPageChange={handleChangeRowsPerPage}
-					labelRowsPerPage="Categorias por página:"
-				/>
-			</Paper>
+													}
+													label={row.enable ? 'Inativar' : 'Ativar'}
+												/>
+											</FormGroup>
+										</div>
+									</TableCell>
+								</TableRow>
+							))
+						)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<TablePagination
+				rowsPerPageOptions={[5, 10, 25]}
+				component="div"
+				count={sortedProducts.length}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				onPageChange={handleChangePage}
+				onRowsPerPageChange={handleChangeRowsPerPage}
+				labelRowsPerPage="Categorias por página:"
+			/>
 		</Box>
 	);
 }
