efdb0171e5917443cb9419876ea55c9b30edcdbf
misasoares <misasoaresms2@gmail.com>
2024-02-23 08:26:38 -0300

    feature: configura caminho para transitar os dadods entre componentes

--- src/app/components/ValueAndDueDate.tsx
+++ src/app/components/ValueAndDueDate.tsx
@@ -2,22 +2,24 @@ import { InputAdornment, TextField } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { ptBR } from 'date-fns/locale';
+import { ChangeEvent } from 'react';
 
 interface ValueAndDueDateProps {
-	setFormData: (arg: any) => void;
-	formData: any;
+	setFormDataValue: (arg: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+	formData: { value: string; dueDate: Date | null };
+	setFormDataDueDate: (arg: Date) => void;
 }
 
-export default function ValueAndDueDate({ setFormData, formData }: ValueAndDueDateProps) {
+export default function ValueAndDueDate({ setFormDataValue, formData, setFormDataDueDate }: ValueAndDueDateProps) {
 	const currentDate = new Date();
 	const minDate = new Date();
 	minDate.setDate(currentDate.getDate() + 7);
 	return (
-		<div className="flex flex-col sm:flex-row items-center gap-24">
+		<div className="flex w-full flex-col sm:flex-row items-center gap-24">
 			<TextField
-				onChange={e => setFormData(prevState => ({ ...prevState, totalValue: e.target.value }))}
+				onChange={e => setFormDataValue(e)}
 				className="w-full"
-				value={formData.totalValue}
+				value={formData.value}
 				type="number"
 				label="Valor"
 				InputProps={{
@@ -36,7 +38,7 @@ export default function ValueAndDueDate({ setFormData, formData }: ValueAndDueDa
 					value={formData.dueDate}
 					minDate={minDate}
 					format="dd/MM/yyyy"
-					onChange={d => setFormData(prevState => ({ ...prevState, dueDate: d }))}
+					onChange={e => setFormDataDueDate(e)}
 				/>
 			</LocalizationProvider>
 		</div>
