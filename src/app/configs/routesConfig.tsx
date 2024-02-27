50589f44841ac9878e51eab857b8ca326a1ccb13
misasoares <misasoaresms2@gmail.com>
2024-02-27 13:59:43 -0300

    doc: remove itens que não são utilizados

--- src/app/configs/routesConfig.tsx
+++ src/app/configs/routesConfig.tsx
@@ -6,13 +6,9 @@ import { Navigate } from 'react-router-dom';
 import Error404Page from '../main/404/Error404Page';
 import authenticationConfig from '../main/authentication/authenticationConfig';
 import categoriesPageConfig from '../main/categories/CategoriesPageConfig';
-import PaymentRequestFormGeneral from '../main/form-request/FormRequest';
 import paymentRequestFormGeneralPageConfig from '../main/form-request/FormRequestConfig';
-import RequestsPage from '../main/requests/RequestsPage';
 import requestPageConfig from '../main/form-request/RequestConfig';
-import CreateProductsPage from '../main/products/CreateProductsPage';
 import CreateProductsPageConfig from '../main/products/CreateProductsPageConfig';
-import ProductsPage from '../main/products/ProductsPage';
 import ProductsPageConfig from '../main/products/ProductsPageConfig';
 
 const routeConfigs: FuseRouteConfigsType = [
@@ -45,22 +41,6 @@ const routes: FuseRoutesType = [
 	{
 		path: '*',
 		element: <Navigate to="404" />
-	},
-	{
-		path: 'solicitar-pagamento',
-		element: <PaymentRequestFormGeneral />
-	},
-	{
-		path: 'solicitacoes',
-		element: <RequestsPage />
-	},
-	{
-		path: 'cadastrar-produto',
-		element: <CreateProductsPage />
-	},
-	{
-		path: 'produtos',
-		element: <ProductsPage />
 	}
 ];
 
