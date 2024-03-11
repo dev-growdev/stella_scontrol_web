45c4c494f6d6b7d4fe5bb971d04d7f3a3675ffd2
misasoares <misasoaresms2@gmail.com>
2024-03-11 13:20:38 -0300

    feature: adiciona mascara de valor utilizando lib numeral

--- src/app/components/RatiableTable.tsx
+++ src/app/components/RatiableTable.tsx
@@ -1,43 +1,98 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import numeral from 'numeral';
+import { UseFieldArrayRemove } from 'react-hook-form';
 import { CostCenters } from '../main/form-request/FormRequest';
+
+numeral.register('locale', 'br', {
+	delimiters: {
+		thousands: '.',
+		decimal: ','
+	},
+	abbreviations: {
+		thousand: 'k',
+		million: 'm',
+		billion: 'b',
+		trillion: 't'
+	},
+	ordinal(number) {
+		return number === 1 ? 'um' : 'um';
+	},
+	currency: {
+		symbol: 'R$'
+	}
+});
 
 interface RatiableProps {
 	costCenters: CostCenters[];
+	remove: UseFieldArrayRemove;
 }
 
-export default function RatiableTable({ costCenters }: RatiableProps) {
+export default function RatiableTable({ costCenters, remove }: RatiableProps) {
+	numeral.locale('br');
+
+	function formatPrice(value) {
+		return numeral(value).format('0,0.00');
+	}
+
+	function totalValue() {
+		let total = 0;
+
+		costCenters.forEach(costCenter => {
+			const value = parseFloat(costCenter.value.replace(',', '.'));
+
+			if (Number(value)) {
+				total += value;
+			}
+		});
+
+		return numeral(total).format('0,0.00');
+	}
+
 	return (
-		<TableContainer
-			className="my-36"
-			component={Paper}
-			elevation={3}
-		>
-			<Table
-				sx={{ minWidth: 700 }}
-				aria-label="customized table"
+		<div className="">
+			<TableContainer
+				className="my-36"
+				component={Paper}
+				elevation={3}
 			>
-				<TableHead>
-					<TableRow>
-						<TableCell sx={{ backgroundColor: '#ffffff' }}>CENTRO DE CUSTOS</TableCell>
-						<TableCell sx={{ backgroundColor: '#ffffff' }}>CONTA CONTÁBIL</TableCell>
-						<TableCell
-							align="right"
-							sx={{ backgroundColor: '#ffffff' }}
-						>
-							VALOR
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{costCenters.map(item => (
-						<TableRow key={item.costCenter + Math.random()}>
-							<TableCell>{item.costCenter}</TableCell>
-							<TableCell>{item.accountingAccount}</TableCell>
-							<TableCell align="right">{item.value}</TableCell>
+				<Table
+					sx={{ minWidth: 700 }}
+					aria-label="customized table"
+				>
+					<TableHead>
+						<TableRow>
+							<TableCell sx={{ backgroundColor: '#ffffff' }}>CENTRO DE CUSTOS</TableCell>
+							<TableCell sx={{ backgroundColor: '#ffffff' }}>CONTA CONTÁBIL</TableCell>
+							<TableCell sx={{ backgroundColor: '#ffffff' }}>VALOR</TableCell>
+							<TableCell
+								align="right"
+								sx={{ backgroundColor: '#ffffff' }}
+							>
+								AÇÕES
+							</TableCell>
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+					</TableHead>
+					<TableBody>
+						{costCenters.map((item, index) => (
+							<TableRow key={item.costCenter + Math.random()}>
+								<TableCell>{item.costCenter}</TableCell>
+								<TableCell>{item.accountingAccount}</TableCell>
+								<TableCell>R${formatPrice(item.value)}</TableCell>
+								<TableCell className="flex justify-end">
+									<FuseSvgIcon
+										color="primary"
+										onClick={() => remove(index)}
+									>
+										heroicons-outline:trash
+									</FuseSvgIcon>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<Typography className="text-right">Valor total: R${totalValue()}</Typography>
+		</div>
 	);
 }
