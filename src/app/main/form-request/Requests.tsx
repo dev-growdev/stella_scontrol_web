67fbc8ebe4a881c06b91080bdd4f65f830ba90f1
misasoares <misasoaresms2@gmail.com>
2024-03-11 17:03:59 -0300

    feature: cria página de listar requests

--- src/app/main/form-request/Requests.tsx
+++ src/app/main/form-request/Requests.tsx
@@ -1,14 +1,50 @@
-import { Box, Typography } from '@mui/material';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { Box, Button, Paper, Typography } from '@mui/material';
+import { useAppDispatch } from 'app/store';
+import { selectUser } from 'app/store/user/userSlice';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import RequestsTable from '../../components/RequestsTable';
+import { listRequestsPaymentsByUser } from './FormRequestSlice';
 
 export default function Requests() {
+	const navigate = useNavigate();
+	const dispatch = useAppDispatch();
+	const user = useSelector(selectUser);
+
+	useEffect(() => {
+		dispatch(listRequestsPaymentsByUser(user.uid));
+	}, []);
+
 	return (
-		<Box className="flex flex-col w-full">
-			<Typography
-				variant="h1"
-				component="h1"
-			>
-				Solicitações
-			</Typography>
+		<Box className="flex flex-col w-full ">
+			<div className="p-32 mt-20">
+				<Button
+					className="mb-12"
+					variant="text"
+					color="primary"
+					onClick={() => navigate('/solicitar-pagamento')}
+					startIcon={<FuseSvgIcon>material-twotone:arrow_back_ios</FuseSvgIcon>}
+				>
+					ABRIR NOVA SOLICITAÇÃO
+				</Button>
+				<Paper
+					elevation={4}
+					className="p-28"
+				>
+					<Typography
+						className="text-20 md:text-28"
+						component="h1"
+						variant="h4"
+						fontWeight={500}
+						sx={{ color: theme => theme.palette.secondary.main }}
+					>
+						Solicitações
+					</Typography>
+				</Paper>
+				<RequestsTable rows={[{ uid: '19023', name: 'oi' }]} />
+			</div>
 		</Box>
 	);
 }
