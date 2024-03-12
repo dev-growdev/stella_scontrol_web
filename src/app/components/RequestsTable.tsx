c6b6bca9418f7218f161aa2201bf864fbe92dbd6
misasoares <misasoaresms2@gmail.com>
2024-03-12 10:49:08 -0300

    feature: estiliza e imprime os valores na tabela

--- src/app/components/RequestsTable.tsx
+++ src/app/components/RequestsTable.tsx
@@ -1,29 +1,64 @@
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { Chip, Stack, styled, TableCell, tableCellClasses } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import * as React from 'react';
+import { ChangeEvent, useState } from 'react';
+import { RequestType } from '../main/form-request/FormRequestSlice';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+	[`&.${tableCellClasses.head}`]: {
+		color: theme.palette.secondary.dark,
+		backgroundColor: theme.palette.action.hover
+	},
+	[`&.${tableCellClasses.body}`]: {
+		fontSize: 14,
+		color: theme.palette.secondary.dark
+	}
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+	'&:nth-of-type(odd)': {
+		backgroundColor: theme.palette.common.white
+	},
+	'&:nth-of-type(even)': {
+		backgroundColor: theme.palette.action.hover
+	},
+	// hide last border
+	'&:last-child td, &:last-child th': {
+		border: 0
+	}
+}));
 
 interface RequestTableProps {
-	rows: { uid: string; name: string }[];
+	rows: RequestType[];
 }
 
 export default function RequestsTable({ rows }: RequestTableProps) {
-	const [page, setPage] = React.useState(0);
-	const [rowsPerPage, setRowsPerPage] = React.useState(10);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
 
 	const handleChangePage = (event: unknown, newPage: number) => {
 		setPage(newPage);
 	};
 
-	const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
 		setRowsPerPage(+event.target.value);
 		setPage(0);
 	};
+
+	function formatDate(dateParam: Date) {
+		const date = new Date(dateParam);
+		const day = date.getDate();
+		const month = date.getMonth() + 1;
+		const year = date.getFullYear();
+
+		return `${day}/${month}/${year}`;
+	}
 
 	return (
 		<Paper
@@ -36,25 +71,41 @@ export default function RequestsTable({ rows }: RequestTableProps) {
 					aria-label="sticky table"
 				>
 					<TableHead>
-						<TableRow>
-							<TableCell>Data</TableCell>
-							<TableCell>Descrição</TableCell>
-							<TableCell>Forma de pagamento</TableCell>
-							<TableCell>Valor total</TableCell>
-						</TableRow>
+						<StyledTableCell>Tipo</StyledTableCell>
+						<StyledTableCell>Status</StyledTableCell>
+						<StyledTableCell>Data de cadastro</StyledTableCell>
+						<StyledTableCell align="right">Ações</StyledTableCell>
 					</TableHead>
 					<TableBody>
 						{Array.isArray(rows) &&
-							rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+							rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
 								return (
-									<TableRow
+									<StyledTableRow
 										hover
 										role="checkbox"
 										tabIndex={-1}
 										key={row.uid}
 									>
-										<TableCell>{row.name}</TableCell>
-									</TableRow>
+										<StyledTableCell>
+											{index % 2 === 0 ? 'Pagamento geral' : 'Compras'}
+										</StyledTableCell>
+										<StyledTableCell>
+											<Stack
+												direction="row"
+												spacing={1}
+											>
+												<Chip
+													color={index % 2 === 0 ? 'success' : 'primary'}
+													label={index % 2 === 0 ? 'Concluído' : 'Em andamento'}
+												/>
+											</Stack>
+										</StyledTableCell>
+										<StyledTableCell>{formatDate(row.createdAt)}</StyledTableCell>
+										<StyledTableCell className="flex gap-28 justify-end">
+											<FuseSvgIcon color="primary">heroicons-outline:pencil</FuseSvgIcon>
+											<FuseSvgIcon color="primary">heroicons-outline:eye</FuseSvgIcon>
+										</StyledTableCell>
+									</StyledTableRow>
 								);
 							})}
 					</TableBody>
@@ -67,6 +118,7 @@ export default function RequestsTable({ rows }: RequestTableProps) {
 				rowsPerPage={rowsPerPage}
 				page={page}
 				onPageChange={handleChangePage}
+				labelRowsPerPage="Linhas por página"
 				onRowsPerPageChange={handleChangeRowsPerPage}
 			/>
 		</Paper>
