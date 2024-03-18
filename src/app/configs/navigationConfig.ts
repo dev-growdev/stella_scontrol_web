b1f1beac8f5e88d23fca8cd5973c44b2b86dfe3f
Diener Dornelas <diener.ld@outlook.com>
2024-03-18 13:51:44 -0300

    refactor: change logic to see navbar

--- src/app/configs/navigationConfig.ts
+++ src/app/configs/navigationConfig.ts
@@ -1,4 +1,3 @@
-import { FuseNavigationType } from '@fuse/core/FuseNavigation/types/FuseNavigationType';
 import i18next from 'i18next';
 import ar from './navigation-i18n/ar';
 import en from './navigation-i18n/en';
@@ -14,6 +13,10 @@ i18next.addResourceBundle('ar', 'navigation', ar);
  * The navigationConfig object is an array of navigation items for the Fuse application.
  */
 
-const navigationConfig: FuseNavigationType = [...SControlNavigationConfig, ...SQualityNavigationConfig];
+const navigationConfig = {
+	scontrol: SControlNavigationConfig,
+	squality: SQualityNavigationConfig
+};
 
-export default navigationConfig;
+const path = window.location.pathname.split('/')[1];
+export default navigationConfig[path] || SControlNavigationConfig;
