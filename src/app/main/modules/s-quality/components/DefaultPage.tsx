54a9e2a91bedbad03f672adc7303249723894308
Diener Dornelas <diener.ld@outlook.com>
2024-03-27 17:02:51 -0300

    refactor: remove paper

--- src/app/main/modules/s-quality/components/DefaultPage.tsx
+++ src/app/main/modules/s-quality/components/DefaultPage.tsx
@@ -1,18 +1,13 @@
-import { Box, Paper } from '@mui/material';
+import { Box } from '@mui/material';
 
 interface PropsDefaultPage {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function DefaultPage({ children }: PropsDefaultPage) {
-	return (
-		<Box className="p-32 mt-20">
-			<Paper
-				elevation={4}
-				className="mt-24 flex flex-col gap-24 py-16"
-			>
-				{children}
-			</Paper>
-		</Box>
-	);
+  return (
+    <Box className="p-32 mt-20">
+      <Box className="mt-24 flex flex-col gap-10 py-16">{children}</Box>
+    </Box>
+  );
 }
